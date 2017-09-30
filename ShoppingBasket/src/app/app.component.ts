978587c169d8cf2ec998061ddd73af9b6d7e6936
ThusharaSangeetha <thusharasangeetha@gmail.com>
2017-09-30 02:16:31 +0100

    Added list of footwear styles and added a click event.

--- ShoppingBasket/src/app/app.component.ts
+++ ShoppingBasket/src/app/app.component.ts
@@ -2,9 +2,83 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<h1>{{title}}</h1>'+
+  '<h2>Footwear Styles</h2>'+
+  // '<input [(ngModel)] = "item.name" placeholder="Enter name...">'+
+  '<ul class="footies">'+
+  '<li *ngFor = "let footwear of footies" (click)="onSelect(footwear)">'+
+  '  <span class="badge">{{footwear.id}}</span> {{footwear.name}}</li>'+
+  '</ul>',
+ styles: [`
+  .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+  .heroes {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 15em;
+  }
+  .footies li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .footies li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+  }
+  .footies li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+  }
+  .footies .text {
+    position: relative;
+    top: -3px;
+  }
+  .footies .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+`]
+  
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Shopping Items';
+  footies = footweararray;
+  selectedFootwear : Footwear;
+  onSelect(footwear: Footwear): void {
+  this.selectedFootwear = footwear;
 }
+}
+export class Footwear{
+  id: number;
+  name: string;
+}
+
+const footweararray : Footwear[]= [
+{id:1,name:'Wedges'},
+{id:2,name:'Boots'},
+{id:3,name:'Ballerinas'},
+{id:4,name:'Flats'},
+{id:5,name:'High Heels'}
+];
+
